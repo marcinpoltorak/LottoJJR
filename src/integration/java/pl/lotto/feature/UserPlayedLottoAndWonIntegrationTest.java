3e50e6a77715587f37d368ae7b78ee6873b0afe0
martinosso <103454347+martinosso@users.noreply.github.com>
2024-07-31 20:49:14 +0200

    base integration tests and integration package

---
+++ src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -0,0 +1,12 @@
+package pl.lotto.feature;
+
+import org.junit.jupiter.api.Test;
+import pl.lotto.BaseIntegrationTest;
+
+public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void should_user_win_and_system_should_generate_winners(){
+
+    }
+}
