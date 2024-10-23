72390e17ed2f2a214014ef68f89b970cbde78799
martinosso <103454347+martinosso@users.noreply.github.com>
2024-10-23 13:02:01 +0200

    added adjustable clock overriding in integration test

--- src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -34,7 +34,7 @@ public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
         // then
         //step 2: system fetched winning numbers for draw date: 19.11.2022 12:00
         // given
-        LocalDateTime drawDate = LocalDateTime.of(2024, 10, 26, 12, 0, 0);
+        LocalDateTime drawDate = LocalDateTime.of(2022, 11, 19, 12, 0, 0);
         // when && then
         await()
                 .atMost(Duration.ofSeconds(20))
